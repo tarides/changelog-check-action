4cbb91c55a3103add74f3d2cc9051921d04c831a
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2023-05-30 10:58:18 +0200

    print to stderr

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ else
     # a changelog check is required
     # fail if the diff is empty
 
-    echo "User-visible changes should come with an entry in the changelog. This behavior
+    >&2 echo "User-visible changes should come with an entry in the changelog. This behavior
 can be overridden by using the \"no changelog\" label, which is used for changes
 that are not user-visible."
 
